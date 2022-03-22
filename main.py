08300b183a035d35e6d07fcf946512af3421d846
OscarGutierrez312 <49107838+OscarGutierrez312@users.noreply.github.com>
2022-03-21 19:24:18 -0500

    Ventana

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tkinter as Tk
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Protocolo():
+    def __init__(self, root):
+        self.__root = root
+        self.read_data()
+
+    def read_data(self):
+        self.frame = Tk.Frame(self.__root)
+        Tk.Label(self.frame,
+                 text="Ingrese el mensaje",
+                 font=("Arial", 15)).grid(row=0)
+
+        self.input = Tk.Entry(self.frame, width=25)
+
+        self.input.grid(row=0, column=1)
+
+        Tk.Button(self.frame,
+                  text='Enter',
+                  font=("Arial", 12),
+                  command=self.run).grid(row=3,
+                                     column=1,
+                                     sticky=Tk.W,
+                                     pady=4)
+
+        self.frame.pack(pady=150)
+
+    def read(self):
+        return str(self.input.get())
+
+    def run(self):
+        str = self.read()
+        self.clear()
+        self.print(self.transform(str))
+
+    def print(self, data):
+        self.config(data)
+        self.axis.plot(data[0], data[1])
 
 
-# Press the green button in the gutter to run the script.
+    def clear(self):
+        for widget in root.winfo_children():
+            widget.destroy()
+        self.frame.pack_forget()
+
+    def restart(self):
+        self.clear()
+        self.read_data()
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    root = Tk.Tk()
+    screen = Protocolo(root)
+    root.title("Protocolo de Enlace de Datos")
+    root.geometry("700x700")
+    root.update()
+    root.mainloop()
